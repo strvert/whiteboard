970015036c5719bbb6599102c495dc323c5ce8e0
strvworks <strv13570@gmail.com>
2018-05-19 05:08:17 +0900

    update

--- plugins/wether_mentions.py
+++ plugins/wether_mentions.py
@@ -2,83 +2,17 @@
 
 from slackbot.bot import respond_to
 from openweathermap import Openweather
-import re
-from datetime import datetime
-from datetime import timedelta
+
 
 
 @respond_to(r'天気|てんき|テンキ|weather')
 def wether_mention(message):
     msg = message.body['text']
-    today = datetime.today()
-    tomorrow_calls = '明日|翌日|あした|アシタ|あす|tomorrow'
-    today_calls = '今日|きょう|today'
-    dat_calls = '明後日|あさって'
-    tda_calls = '明々後日|しあさって'
-    day_calls = '{0}|{1}|{2}|{3}'.format(
-            today_calls, tomorrow_calls, dat_calls, tda_calls)
-    msg_day = re.findall(day_calls, msg)
-    weatherapi = Openweather()
-    city = weatherapi.getCityname()
 
-    date_str = ''
-    try:
-        if msg_day[0] == '今日' or msg_day[0]=='きょう':
-            today += timedelta(days=0)
-            date_str = "{0:02d}{1:02d}".format(
-                    today.month, today.day
-                    )
-            md_str = "{0}月{1}日".format(today.month, today.day)
-            if datetime.today().hour >= 21:
-                message.reply(
-                        "21時以降は当日の天気予報情報を取得できません。" \
-                        + "ごめんなさい。"
-                        )
-                return
-        elif msg_day[0]=='明日' or msg_day[0]=='あす' \
-                or msg_day[0]=='あした' or msg_day[0]=='アシタ':
-            today += timedelta(days=1)
-            date_str = "{0:02d}{1:02d}".format(
-                    today.month, today.day
-                    )
-            md_str = "{0}月{1}日".format(today.month, today.day)
-        elif msg_day[0] == '明後日' or msg_day[0] == 'あさって':
-            today += timedelta(days=2)
-            date_str = "{0:02d}{1:02d}".format(
-                    today.month, today.day
-                    )
-            md_str = "{0}月{1}日".format(today.month, today.day)
-        elif msg_day[0] == '明々後日' or msg_day[0] == 'しあさって':
-            today += timedelta(days=3)
-            date_str = "{0:02d}{1:02d}".format(
-                    today.month, today.day
-                    )
-            md_str = "{0}月{1}日".format(today.month, today.day)
-    except:
-        today += timedelta(days=0)
-        date_str = "{0:02d}{1:02d}".format(
-                today.month, today.day
-                )
-        md_str = "{0}月{1}日".format(today.month, today.day)
-        if datetime.today().hour >= 21:
-            message.reply(
-                    "21時以降は当日の天気予報情報を取得できません。" \
-                    + "ごめんなさい。"
-                    )
-            return
 
-    reply_str =  "\n" + md_str + "の" + city + "の天気をお伝えします。\n"
-
-    try:
-        weatherdata = weatherapi.getOnedayweather(date_str)
-        for datum in weatherdata:
-            reply_str += datum['time_str'] + "\t"
-            reply_str += datum['japanese_main'] + "\n"
-    except:
-        message.reply(
-                "取得中にエラーが発生しました。" \
-                + "連続する場合は開発者に連絡ください。"
-                )
-        return
+    weatherdata = weatherapi.getOnedayweather(date_str)
+    for datum in weatherdata:
+        reply_str += datum['time_str'] + "\t"
+        reply_str += datum['japanese_main'] + "\n"
 
     message.reply(reply_str)
--- plugins/word_to_date.py
+++ plugins/word_to_date.py
@@ -1,72 +1,82 @@
 # coding: utf-8
 
 import re
-from datetime import datetime
+from datetime import date
 from datetime import timedelta
+import mojimoji
+
 
 class WordToDate:
 
     def __init__(self):
-        self.tomorrow_words = '明日|翌日|あした|アシタ|あす|tomorrow'
         self.today_words = '今日|きょう|today'
-        self.dat_words = '明後日|あさって'
-        self.tda_words = '明々後日|しあさって'
+        self.tomorrow_words = '明日|翌日|あした|アシタ|あす|tomorrow'
+        self.in_two_days_words = '明後日|あさって'
+        self.in_three_days_words = '明々後日|しあさって'
         self.day_words = '{0}|{1}|{2}|{3}'.format(
-                self.today_words, self.tomorrow_words, self.dat_words,
-                self.tda_words)
-        self.today_date = datetime.today()
-        self.tomorrow_date = datetime.today() + timedelta(days=1)
-        self.dat_date = datetime.today() + timedelta(days=2)
-        self.tda_date = datetime.today() + timedelta(days=3)
+            self.today_words, self.tomorrow_words, self.in_two_days_words, self.in_three_days_words)
+        self.today_date = date.today()
+        self.tomorrow_date = date.today() + timedelta(days=1)
+        self.in_two_days_date = date.today() + timedelta(days=2)
+        self.in_three_days_date = date.today() + timedelta(days=3)
 
-    def getDaywords(self):
+    def getDayWords(self):
         return self.day_words
 
-    def getTomorrowwords(self):
-        return self.tomorrow_words
-
-    def getDatwords(self):
-        return self.dat_words
-
-    def getTdawords(self):
-        return self.tda_words
-
-    def getTodaywords(self):
+    def getTodayWords(self):
         return self.today_words
 
-    def findDaywords(self, msg, getall = False):
-        day_words = re.findall(self.day_words, msg)
-        if getall:
-            return day_words
-        else:
-            return day_words[0]
+    def getTomorrowWords(self):
+        return self.tomorrow_words
 
-    def wordToAfterday(self, day_word):
-        if re.search(self.today_words, day_word):
-            date_str = "{0:02d}{1:02d}".format(
-                    self.today.month, self.today.day
-                    )
-            md_str = "{0}月{1}日".format(today.month, today.day)
-            if datetime.today().hour >= 21:
-                message.reply(
-                        "21時以降は当日の天気予報情報を取得できません。" \
-                        + "ごめんなさい。"
-                        )
-                return
-        elif re.search(self.tomorrow_words, day_word):
-            date_str = "{0:02d}{1:02d}".format(
-                    self.tomorrow_date.month, self.tomorrow_date.day 
-                    )
-            md_str = "{0}月{1}日".format(today.month, today.day)
-        elif day_word == '明後日' or day_word == 'あさって':
-            today += timedelta(days=2)
-            date_str = "{0:02d}{1:02d}".format(
-                    today.month, today.day
-                    )
-            md_str = "{0}月{1}日".format(today.month, today.day)
-        elif day_word == '明々後日' or day_word == 'しあさって':
-            today += timedelta(days=3)
-            date_str = "{0:02d}{1:02d}".format(
-                    today.month, today.day
-                    )
-            md_str = "{0}月{1}日".format(today.month, today.day)
+    def getInTwoDaysWords(self):
+        return self.in_two_days_words
+
+    def getInThreeDaysWords(self):
+        return self.in_three_days_words
+
+    def findDaywords(self, msg):
+        return re.findall(self.day_words, msg)[0]
+
+    def wordToDate(self, day_word):
+        if re.search('(?:[0-9]|[０-９]){1,2}月(?:[0-9]|[０-９]){1,2}日', day_word):
+            day_word_list = re.findall('((?:[0-9]|[０-９]){1,2})月((?:[0-9]|[０-９]){1,2})日', day_word)[0]
+            try:
+                return date(date.today().year,
+                            int(mojimoji.zen_to_han(day_word_list[0])), int(mojimoji.zen_to_han(day_word_list[1])))
+            except:
+                return None
+
+        elif re.search('(?:[0-9]|[０-９]){1,2}[/／](?:[0-9]|[０-９]){1,2}', day_word):
+            day_word_list = re.findall('((?:[0-9]|[０-９]){1,2})[/／]((?:[0-9]|[０-９]){1,2})', day_word)[0]
+            try:
+                return date(date.today().year,
+                            int(mojimoji.zen_to_han(day_word_list[0])), int(mojimoji.zen_to_han(day_word_list[1])))
+            except:
+                return None
+
+        elif re.search('(?:[0-9]|[０-９])+日後', day_word):
+            day_word_list = re.findall('((?:[0-9]|[０-９])+)日後', day_word)
+            try:
+                return self.today_date + timedelta(days=int(day_word_list[0]))
+            except:
+                return None
+
+        elif re.search('(?:[0-9]|[０-９])+日前', day_word):
+            day_word_list = re.findall('((?:[0-9]|[０-９])+)日前', day_word)
+            try:
+                return self.today_date - timedelta(days=int(day_word_list[0]))
+            except:
+                return None
+
+        elif re.search(self.day_words, day_word):
+            if re.search(self.in_three_days_words, day_word):
+                return self.in_three_days_date
+            elif re.search(self.in_two_days_words, day_word):
+                return self.in_two_days_date
+            elif re.search(self.tomorrow_words, day_word):
+                return self.tomorrow_date
+            elif re.search(self.today_words, day_word):
+                return self.today_date
+        else:
+            return None
